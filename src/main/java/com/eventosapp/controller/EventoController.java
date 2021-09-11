bba000ec89852314673986076faff9217ba6cd61
Roberto Dantas Cajueiro <beto.cajueiro@gmail.com>
2021-09-11 15:33:44 -0300

    Salvando os dados no banco de dados  pelo form

--- src/main/java/com/eventosapp/controller/EventoController.java
+++ src/main/java/com/eventosapp/controller/EventoController.java
@@ -1,14 +1,30 @@
 package com.eventosapp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.eventosapp.models.Evento;
+import com.eventosapp.repository.EventoRepository;
 
 @Controller
 public class EventoController {
 	
-	@RequestMapping("/cadastrarEvento")
+	@Autowired
+	private EventoRepository eventoRepository; 
+	
+	@RequestMapping(value = "/cadastrarEvento", method=RequestMethod.GET)
 	public String form() {
 		return "evento/formEvento";
+	}
+	
+	@RequestMapping(value = "/cadastrarEvento", method=RequestMethod.POST)
+	public String form(Evento evento) {
+		
+		eventoRepository.save(evento);
+		
+		return "redirect:/cadastrarEvento";
 	}
 
 }
