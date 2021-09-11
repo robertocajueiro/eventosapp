33acb33d3a36291c817d3fc0432664e07c805ef0
Roberto Dantas Cajueiro <beto.cajueiro@gmail.com>
2021-09-11 18:43:59 -0300

    Criando requisições especificas com a anotação @PathValiable

--- src/main/java/com/eventosapp/controller/EventoController.java
+++ src/main/java/com/eventosapp/controller/EventoController.java
@@ -2,6 +2,7 @@ package com.eventosapp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -36,6 +37,14 @@ public class EventoController {
 		mv.addObject("eventos", eventos);
 		return mv;
 		
+	}
+	
+	@RequestMapping("/{codigo}")
+	public ModelAndView detalhesEvento(@PathVariable("codigo") long codigo) {
+		Evento evento = eventoRepository.findByCodigo(codigo);
+		ModelAndView mv = new ModelAndView("evento/detalhesEvento");
+		mv.addObject("evento", evento);
+		return mv;
 	}
 
 }
--- src/main/java/com/eventosapp/repository/EventoRepository.java
+++ src/main/java/com/eventosapp/repository/EventoRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 import com.eventosapp.models.Evento;
 
 public interface EventoRepository extends CrudRepository<Evento, Long>{
-
+	
+	Evento findByCodigo(long codigo);
 }
